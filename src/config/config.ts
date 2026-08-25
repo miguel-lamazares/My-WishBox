c17088e40aac96a5ec8992ebc32e23e9e9981c85
Miguel Lamazares <miguellamazares8@gmail.com>
2026-08-24 21:40:35 -0300

    docker working lol

--- src/config/config.ts
+++ src/config/config.ts
@@ -13,17 +13,8 @@ export const config = {
 
     databaseUrl:
         process.env.DATABASE_URL ??
-        "postgresql://wishbox:noob@127.0.0.1:5432/wishbox",
+        "postgresql://wishbox:wishbox_password@localhost:5432/wishbox",
 
-    /*
-     * O bot usa o próprio número do WhatsApp.
-     *
-     * Portanto mensagens fromMe são permitidas por padrão.
-     *
-     * Para bloquear:
-     *
-     * ALLOW_FROM_ME=0
-     */
     allowFromMe:
         process.env.ALLOW_FROM_ME !== "0",
 
